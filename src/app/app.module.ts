0f943d1b84679436394e08f14440c117686eeda9
rameezv <rameez@bigtreeworld.com>
2018-10-28 21:23:31 -0600

    Recaptcha on contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule } from '@angular/material';
+import { RecaptchaModule } from 'ng-recaptcha';
+import { RecaptchaFormsModule } from 'ng-recaptcha/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderBarComponent } from './components/header-bar/header-bar.component';
 import { OverviewComponent } from './components/overview/overview.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './components/footer/footer.component';
 import { ServicesComponent } from './components/services/services.component';
 import { ContactService } from './services/contact/contact.service';
@@ -37,6 +39,8 @@ const materialModules = [
     BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    RecaptchaModule,
+    RecaptchaFormsModule,
     materialModules
   ],
   providers: [
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -9,14 +9,18 @@ import { ContactService } from '../../services/contact/contact.service';
 })
 export class ContactComponent implements OnInit {
   form: FormGroup;
+  reCaptchaSiteKeyEncrypted = 'NkxjclZYY1VBQUFBQVAxWTlhMXpxTHBmc1FxVXJNNTlERF9Tclc0YQ==';
+  reCaptchaSiteKey: string;
 
   constructor(private fb: FormBuilder, private contactService: ContactService) {}
   ngOnInit() {
+    this.reCaptchaSiteKey = window.atob(this.reCaptchaSiteKeyEncrypted);
     this.form = this.fb.group({
       'name': new FormControl(null, Validators.required),
       'email': new FormControl(null, [Validators.email, Validators.required]),
       'phone': new FormControl(null, Validators.pattern(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)),
-      'message': new FormControl(null, Validators.required)
+      'message': new FormControl(null, Validators.required),
+      'recaptcha': new FormControl(null, Validators.required)
     });
   }
 
