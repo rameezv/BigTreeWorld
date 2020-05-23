9b9eb21bbfc8f9ad480079786c32e7e25989cb6b
Rameez Virji <rameez@bigtreeworld.com>
2020-05-23 15:18:01 -0600

    Fix sendgrid integration

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,38 +1,19 @@
 import * as functions from 'firebase-functions';
-import * as sendgrid from 'sendgrid';
-import * as cors from 'cors';
-const corsHandler = cors({origin: true});
-
-const sendGridClient = sendgrid(functions.config().sendgrid.apikey);
-
-function parseBody(body) {
-  const helper = sendgrid.mail;
-  const fromEmail = new helper.Email(body.from);
-  const toEmail = new helper.Email(body.to);
-  const subject = body.subject;
-  const message = new helper.Content('text/html', body.message);
-  const mail = new helper.Mail(fromEmail, subject, toEmail, message);
-  return mail.toJSON();
-}
+import * as sendgrid from '@sendgrid/mail';
 
 export const httpEmail = functions.https.onRequest((req, res) => {
-  corsHandler(req, res, () => {
-    if (req.method !== 'POST') {
-      return Promise.resolve().then(() => {
-        return res.status(405).send('Only POST requests are accepted');
-      });
-    }
-
-    const request = sendGridClient.emptyRequest({
-      method: 'POST',
-      path: '/v3/mail/send',
-      body: parseBody(req.body)
-    });
-
-    return sendGridClient.API(request).then(() => {
-      return res.status(200).send({'message': 'Email sent successfully'});
-    }).catch(err => {
-      return res.status(500).send(err.message);
-    });
+  sendgrid.setApiKey(functions.config().sendgrid.apikey);
+  const msg = {
+    to: req.body.to,
+    from: req.body.from,
+    subject: req.body.subject,
+    html: req.body.message,
+  };
+  sendgrid.send(msg).then(() => {
+    return res.status(200).send({'message': 'Email sent successfully'});
+  }).catch(err => {
+    return res.status(500).send(err.message);
   });
-})
+
+  return {'message': 'Email sent successfully'};
+});
